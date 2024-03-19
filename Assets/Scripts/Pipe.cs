a64b267ebde48ff2f220bf94d25afdfb929071f7
Letícia Brudeck <101845889+leticiabrus@users.noreply.github.com>
2024-03-18 21:23:37 -0300

    Atualização

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -13,5 +13,13 @@ public class Pipe : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.left * Time.deltaTime * speed;
+
+        if (transform.position.x <-2.5f)
+        {
+
+            // AUTODESTRUIR
+
+            Destroy(gameObject);
+        }
     }
 }
